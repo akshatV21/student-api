36886c9c36f72241c76b74b5ab182dcb2b3f03d5
akshatV21 <a.vishwakarma2110@gmail.com>
2022-10-18 19:55:29 +0530

    added env

--- src/mongo.js
+++ src/mongo.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const { default: mongoose } = require('mongoose')
 
 const connectToMongoDB = async () => {
-  await mongoose.connect('mongodb+srv://akshat21:aku1985pika@cluster0.ew0oz.mongodb.net/kmart?retryWrites=true&w=majority')
+  await mongoose.connect(process.env.MONGO_URL)
 }
 
 const disconnectToMongoDB = async () => {
